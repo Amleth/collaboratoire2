cdac9534e6012fbfbaa2159f30ac5fe838b4e4fc
Amleth <4mleth@gmail.com>
2017-12-14 10:28:30 +0100

    No more commented menu

--- app/main.dev.js
+++ app/main.dev.js
@@ -74,10 +74,6 @@ app.on('ready', async () => {
     mainWindow = null;
   });
 
-  // const menuBuilder = new MenuBuilder(mainWindow);
-  // menuBuilder.buildMenu();
-  // mainWindow.setMenu(null);
-
   if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true') {
     mainWindow.toggleDevTools();
   }
