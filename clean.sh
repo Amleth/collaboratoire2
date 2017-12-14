603d03b0ccef932d5c887ae2f1dead7975589702
Amleth <4mleth@gmail.com>
2017-12-14 10:34:19 +0100

    CLEAN script

--- clean.sh
+++ clean.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-rm -rf node_modules
-rm yarn.lock
-rm package-lock.json
-yarn cache clean
 rm -rf ./app/dist
-yarn
-./node_modules/.bin/electron-rebuild
-
+rm ./app/main.prod.js.map
+rm ./app/main.prod.js
+rm -rf ./release
