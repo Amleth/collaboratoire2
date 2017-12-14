5286d8ea56afc411d42060585ffcce01ac824179
Amleth <4mleth@gmail.com>
2017-12-14 09:54:53 +0100

    Get rid of redux-persist

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -1,8 +1,6 @@
 import { createBrowserHistory } from 'history';
 import { routerMiddleware } from 'react-router-redux';
 import { combineReducers, createStore, applyMiddleware } from 'redux';
-// import { persistStore, persistCombineReducers } from 'redux-persist';
-// import storage from 'redux-persist/es/storage';
 import thunk from 'redux-thunk';
 
 import rootReducer from '../reducers';
@@ -12,13 +10,9 @@ const router = routerMiddleware(history);
 const enhancer = applyMiddleware(thunk, router);
 
 function configureStore(initialState) {
-  // const config = { key: 'root', storage };
-  // const store = createStore(persistCombineReducers(config, rootReducer), initialState, enhancer);
   const store = createStore(combineReducers(rootReducer), initialState, enhancer);
-  // const persistor = persistStore(store);
 
-  // return { persistor, store };
-  return { store };
+  return store;
 }
 
 export default { configureStore, history };
